134e2e10e45f107c2bd52fd79bec425dd148ad6c
Zakaria E Heddadi <zakaria.elheddadi@gmail.com>
2021-04-16 14:47:49 +0000

    update

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const debug = require('debug')('crawler');
 const puppeteer = require('puppeteer');
 const commander = require('commander');
-
+const { MongoClient } = require('mongodb');
 const csv = require('./lib/export-csv');
 const StackOverflowJobs = require('./lib/jobs');
+require('dotenv').config();
+
+// Connection URI
+const uri = process.env.DB_PATH;
+// Create a new MongoClient
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 commander
+  .option('-p, --numberOfPage', 'Page (eg: 20)')
   .option('-f, --file', 'File (eg: jobs.csv)')
   .option('-l, --location', 'The location where to find a job (eg: Amsterdam)')
   .parse(process.argv);
@@ -14,6 +24,12 @@ debug.enabled = true;
 
 async function run(location, filePath) {
   try {
+    await client.connect();
+    // Establish and verify connection
+    const database = client.db('admin');
+
+    const scraping = database.collection('scraping');
+
     debug('Starting puppeteer');
 
     const browser = await puppeteer.launch({
@@ -27,9 +43,13 @@ async function run(location, filePath) {
     const link = await so.getLinks(location);
     const jobs = await so.getJobs(link);
 
-    csv(filePath, jobs);
+    const options = { ordered: true };
+    const result = await scraping.insertMany(jobs, options);
 
-    debug('Exported jobs to %s', filePath);
+    // csv(filePath, jobs);
+    //debug('Exported jobs to %s', filePath);
+    debug(`${result.insertedCount} documents were inserted`);
+
     console.log('\n');
 
     await page.close();
@@ -39,7 +59,7 @@ async function run(location, filePath) {
   }
 }
 
-if (commander.args.length !== 2) {
+if (commander.args.length !== 3) {
   commander.help();
   console.log('\n');
 } else {
